014c09e5e18573dd6059d1890806cf5b7a8dde03
Nano1412 <nano.khongsri@gmail.com>
2023-03-29 14:44:24 +0700

    V3 change print system to function

--- Code/Program.cs
+++ Code/Program.cs
@@ -6,32 +6,25 @@ class Program{
     int K = int.Parse(Console.ReadLine());
     int k1 = K;
     int k2 = K;
-    int order = 1;
 
-    while(N>0){
+    for(int order = 1; order <= N;order++){
       int gender = int.Parse(Console.ReadLine());
       int k1Score = int.Parse(Console.ReadLine());
       int k2Score = int.Parse(Console.ReadLine());
       if(SingerProfile(gender,k1Score,k2Score)){
-        if(k1 > 0){
-          Console.WriteLine("{0} {1}",order,1);
-          k1--;
-        } else if (k2 > 0 && k2Score >= k1Score){
-          Console.WriteLine("{0} {1}",order,2);
-          Console.WriteLine("LOL");
-          k2--;
-        }
+        PrintWinner(order,k1Score,ref k1, 1, k2Score, ref k2, 2);
       } else {
-        if(k2 > 0){
-          Console.WriteLine("{0} {1}",order,2);
-          k2--;
-        } else if (k1 > 0 && k1Score >= k2Score){
-          Console.WriteLine("{0} {1}",order,1);
-          k1--;
-        }
+        PrintWinner(order,k2Score,ref k2, 2, k1Score, ref k1, 1);
       }
-      N--;
-      order++;
+    }
+  }
+  static void PrintWinner(int order, int primary_score, ref int primary_space, int primary_number, int secondary_score,ref int secondary_space, int secondary_number){
+    if(primary_space > 0){
+      Console.WriteLine("{0} {1}",order,primary_number);
+      primary_space--;
+    } else if (secondary_space > 0 && secondary_score >= primary_score){
+      Console.WriteLine("{0} {1}",order,secondary_number);
+      secondary_space--;
     }
   }
   static bool SingerProfile(int gender, int k1Score, int k2Score){
