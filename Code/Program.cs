b8261efee561894d0a33710c97011a4c3498ea22
Nano1412 <nano.khongsri@gmail.com>
2023-03-29 14:25:47 +0700

    revert to V1

--- Code/Program.cs
+++ Code/Program.cs
@@ -6,35 +6,35 @@ class Program{
     int K = int.Parse(Console.ReadLine());
     int k1 = K;
     int k2 = K;
+    int order = 1;
 
-    for(int order = 1; order <= N; order++){
+    while(N>0){
       int gender = int.Parse(Console.ReadLine());
       int k1Score = int.Parse(Console.ReadLine());
       int k2Score = int.Parse(Console.ReadLine());
-      if(SingerGotPickByWho(gender,k1Score,k2Score)){
-        if(k1 > 0 && k1Score >= k2Score && k1Score >=9){
-          Console.WriteLine("{0} 1",order);
+      if(SingerProfile(gender,k1Score,k2Score)){
+        if(k1 > 0 && k1Score >= k2Score){
+          Console.WriteLine("{0} {1}",order,1);
           k1--;
-        } else if (k2 > 0 && k2Score >= k1Score && k2Score >=9){
-          Console.WriteLine("{0} 2",order);
+        } else if (k2 > 0 && k2Score >= k1Score){
+          Console.WriteLine("{0} {1}",order,2);
           k2--;
         }
       } else {
-        if(k2 > 0 && k2Score >= k1Score && k2Score >=9){
-          Console.WriteLine("{0} 2",order);
+        if(k2 > 0 && k2Score >= k1Score){
+          Console.WriteLine("{0} {1}",order,2);
           k2--;
-        } else if (k1 > 0 && k1Score >= k2Score && k1Score >=9){
-          Console.WriteLine("{0} 1",order);
+        } else if (k1 > 0 && k1Score >= k2Score){
+          Console.WriteLine("{0} {1}",order,1);
           k1--;
         }
       }
+      N--;
+      order++;
     }
   }
+  static bool SingerProfile(int gender, int k1Score, int k2Score){
 
-  static bool IsThereTheSpaceForSinger(int space, int domScore, int minerScore){
-    return space > 0 && domScore >= minerScore && domScore >=9;
-  }
-  static bool SingerGotPickByWho(int gender, int k1Score, int k2Score){
     if((k1Score >=9 && k2Score < 9) || (k1Score >=9 && k2Score >= 9 && gender == 1)){
       return true;
     }
